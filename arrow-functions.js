905c5977aec4d79af0c6ba7bf990dcfab7fce458
Anna Teilane <kastekastee@gmail.com>
2024-12-18 22:10:20 +0000

    inital commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+/*
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+*/
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
 
+let sum = addTwoNumbers(3, 8);
+console.log(sum);
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a ,b) => a + b;
+let sum2 = addTwoNumbers2(6, 4)
+console.log(sum2)
+// const addTwoNumbers2 = (a ,b) => a + b; also valid
 // Implicit Returns
-
+const saySomething = message => console.log(message);
+saySomething('Heyaaaaaa!');
+const sayHello = () => console.log('hello');
+sayHello();
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `hethee
+    heheheee
+    hhhhheeee`
+)
+console.log(returnMultipleLines());
